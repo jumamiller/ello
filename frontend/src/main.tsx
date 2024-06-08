7d1c7dd7061c6ec6380374cecea79f73927006b8
Jakabos <jumamiller@yahoo.com>
2024-06-08 10:54:13 +0300

    setupt the shared theme provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {RouterProvider} from "react-router-dom";
+import {router} from "./router";
+import { Provider } from 'react-redux';
+import {store} from "./store";
+import ThemeProvider from "./shared/theme";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <React.StrictMode>
+        <ThemeProvider>
+            <Provider store={store}>
+                <RouterProvider router={router}/>
+            </Provider>
+        </ThemeProvider>
+    </React.StrictMode>,
+);
---
+++ frontend/src/shared/theme/index.tsx
@@ -0,0 +1,39 @@
+import { ReactNode, useMemo } from 'react';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider as MUIThemeProvider, createTheme, StyledEngineProvider } from '@mui/material/styles';
+import palette from './palette';
+import typography from "./typography.tsx";
+import shadows, {customShadows} from "./shadows.tsx";
+import componentsOverride from './overrides';
+interface ThemeProviderProps {
+    children: ReactNode;
+}
+
+const ThemeProvider = ({ children }: ThemeProviderProps) => {
+    const themeOptions = useMemo(
+        () => ({
+            palette,
+            shape: {
+                borderRadius: 8,
+            },
+            typography,
+            shadows,
+            customShadows,
+        }),
+        []
+    );
+
+    const theme = createTheme(themeOptions);
+    theme.components = componentsOverride(theme);
+
+    return (
+        <StyledEngineProvider injectFirst>
+            <MUIThemeProvider theme={theme}>
+                <CssBaseline />
+                {children}
+            </MUIThemeProvider>
+        </StyledEngineProvider>
+    );
+};
+
+export default ThemeProvider;
