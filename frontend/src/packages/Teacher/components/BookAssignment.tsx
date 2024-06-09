f43bc02f51fda5b1dd88269aca79207411902eec
Jakabos <jumamiller@yahoo.com>
2024-06-09 11:19:07 +0300

    add react query

--- frontend/src/packages/Teacher/components/BookAssignment.tsx
+++ frontend/src/packages/Teacher/components/BookAssignment.tsx
@@ -1,7 +1,42 @@
+import {BookCard} from "../../../shared/components/card/BookCard.tsx";
+import {Grid, Paper, useMediaQuery, useTheme} from "@mui/material";
+import {useNavigate} from "react-router-dom";
+
+const CARD_HEIGHT = 180;
+const CARD_WIDTH = 250;
 export default function BookAssignment() {
+    const navigate = useNavigate();
+    const handleCardClick = (url) => {
+        navigate(`/dashboard/${url}`);
+    };
+
+    const theme = useTheme();
+    const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+    const isZoomedIn = useMediaQuery(theme.breakpoints.up('md'));
+
     return (
-        <div>
-            <h1>Book Assignment Page</h1>
-        </div>
+        <Paper
+            sx={{
+                padding: 2,
+                margin: 3
+            }}
+        >
+            <Grid container spacing={2} sx={{ height: '100%' }}>
+                <Grid item xs={12} md={isZoomedIn ? 8 : 12} lg={isZoomedIn ? 8 : 12}
+                      sx={{ backgroundColor: 'secondary.lighter' }}>
+                    <Grid container spacing={isSmallScreen ? 2 : 3}>
+                        {/* First Row */}
+                        <Grid item xs={12} sm={6} md={4}>
+                            <BookCard
+                                name="F.O.S.A ACCOUNT"
+                                height={CARD_HEIGHT}
+                                width={CARD_WIDTH}
+                                onClick={()=>handleCardClick("fosa-account")}
+                            />
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Paper>
     )
 }
--- frontend/src/packages/Teacher/view/TeacherLanding.tsx
+++ frontend/src/packages/Teacher/view/TeacherLanding.tsx
@@ -1,10 +1,12 @@
 import Header from "../components/Header.tsx";
 import {Stack} from "@mui/material";
+import BookAssignment from "../components/BookAssignment.tsx";
 
 export default function TeacherLanding(){
 return (
         <Stack>
             <Header/>
+            <BookAssignment/>
         </Stack>
     )
 }
